96d2b9397c2384bf8e3143e22f01e7cd347e1876
Arundhati <arundhatib.work@gmail.com>
2023-09-20 23:00:55 +0530

    Generating Plan

---
+++ src/GeneratingPlan/GeneratingPlan.js
@@ -0,0 +1,14 @@
+import './GeneratingPlan.css';
+import Question from '../Question/Question';
+
+
+
+function GeneratingPlan(){
+    return (
+        <div className="container">
+            <Question q="Generating Plan..."/> 
+        </div>
+    );
+}
+
+export default GeneratingPlan;
--- src/main-container/MainContainer.js
+++ src/main-container/MainContainer.js
@@ -2,6 +2,7 @@ import Navbar from "../Navbar/Navbar";
 import TimeFrame from "../TimeFrame/TimeFrame";
 import TopicBlock from "../TopicBlock/TopicBlock";
 import SubtopicBlock from "../SubtopicBlock/SubtopicBlock";
+import GeneratingPlan from "../GeneratingPlan/GeneratingPlan";
 import './MainContainer.css';
 
 function MainContainer(){
@@ -10,7 +11,7 @@ function MainContainer(){
             <div className="main-container">
                 <Navbar/>
                 <div className="frame-container">
-                    <TimeFrame/> 
+                    <GeneratingPlan/> 
                 </div>
             </div>
         </div>
